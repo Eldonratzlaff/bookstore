9e82265f6cbf7dc8ff384ba9501bc5bafded91b7
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-05-20 19:23:11 -0400

    create erase reducer

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,18 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    Books: [],
-};
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
 
 const BooksSlice = createSlice({
-    name: 'Books',
-    initialState,
-    reducers: { 
-      addBook: (state, action) => {
-        state.Books.push(action.payload);
-      }         
-    }
+  name: 'Books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      const index = state.findIndex(book => book.item_id === action.payload.item_id);
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
+    },
+  },
 });
+
+export const { addBook, removeBook } = BooksSlice.actions;
 
 export default BooksSlice.reducer;
 
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,16 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit"; 
+import { createSlice } from "@reduxjs/toolkit";
 
 const CategoriesSlice = createSlice({
-    name: 'Categories',
-    initialState: {
-        Categories: [],
-    },      
-    reducers: {
-        addCategory: (state, action) => {
-            state.Categories.push(action.payload);
-        }
-
-    }   
+  name: 'Categories',
+  initialState: {
+    categories: [],
+  },
+  reducers: {
+    addCategory: (state, action) => {
+      state.categories.push(action.payload);
+    },
+  },
 });
 
+export const { addCategory } = CategoriesSlice.actions;
+
 export default CategoriesSlice.reducer;
+
