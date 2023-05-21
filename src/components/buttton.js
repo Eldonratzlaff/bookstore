7810373433605646e9ae1954759703365ebb2caa
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-05-20 22:39:17 -0400

    Add and remove buttons

--- src/components/buttton.js
+++ src/components/buttton.js
@@ -1,18 +1,3 @@
 
 
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../reducers/books';
 
-const Button = ({ bookId }) => {
-  const dispatch = useDispatch();
-
-  const handleRemoveBook = () => {
-    dispatch(removeBook(bookId));
-  };
-
-  return (
-    <button onClick={handleRemoveBook}>Remove Book</button>
-  );
-};
-
-export default Button;
